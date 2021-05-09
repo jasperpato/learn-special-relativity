d0d7c78ca19c8e1e8cad243c152f429756226e46
jasperpato <jasperpato@gmail.com>
2021-05-09 12:04:44 +0800

    animation and centre content in lesson2

--- app/static/script.js
+++ app/static/script.js
@@ -1,40 +1,44 @@
 function init() {
-  scroll();
-  dropMenu();
-  titleParallax();
-  setInterval(animate, 2000);
+	scroll();
+	dropMenu();
+	titleParallax();
+	setInterval(animate, 1000);
 }
 
 function scroll() {
-  let progress = document.getElementById("progressbar");
-  let totalHeight = document.body.scrollHeight - window.innerHeight;
-  window.onscroll = function () {
-    progress.style.height = (window.pageYOffset / totalHeight) * 100 + "%";
-  };
+	let progress = document.getElementById("progressbar");
+	let totalHeight = document.body.scrollHeight - window.innerHeight;
+	window.onscroll = function () {
+		progress.style.height = (window.pageYOffset / totalHeight) * 100 + "%";
+	};
 }
 
 function dropMenu() {
-  $(document).ready(function () {
-    $(".menu-toggle").click(function () {
-      $("nav").toggleClass("active");
-    });
-  });
+	$(document).ready(function () {
+		$(".menu-toggle").click(function () {
+			$("nav").toggleClass("active");
+		});
+	});
 }
 
 function titleParallax() {
-  $("body").mousemove(function (e) {
-    var valueX = (e.pageX * -1) / 75;
-    var valueY = (e.pageY * -1) / 75;
+	$("body").mousemove(function (e) {
+		var valueX = (e.pageX * -1) / 75;
+		var valueY = (e.pageY * -1) / 75;
 
-    $("#title").css({
-      transform: "translate(" + valueX + "px," + valueY + "px)",
-    });
-  });
+		$("#title").css({
+			transform: "translate(" + valueX + "px," + valueY + "px)",
+		});
+	});
 }
 
 function animate() {
-  $("#animate-1").html(
-    '<img class="diagram" src="{{ url_for("static", filename="diagrams/Diagram_1.1.png") }}">' );
-  setTimeout(function() {$("#animate-1").html(
-    '<img class="diagram" src="{{ url_for("static", filename="diagrams/Diagram_1.2.png") }}">')}, 1000);
+	// animation 1 in lesson-2
+	if ($("#animate-1").find("img")[0].id == "img-1") {
+		$("#animate-1").find("img")[0].id = "img-2";
+		$("#animate-1").find("img")[0].src = "/static/diagrams/Diagram_1.2.png";
+	} else {
+		$("#animate-1").find("img")[0].id = "img-1";
+		$("#animate-1").find("img")[0].src = "/static/diagrams/Diagram_1.1.png";
+	}
 }
