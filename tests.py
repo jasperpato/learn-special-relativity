795fcaec63438a9b259b3a6af7ce98955d168c5b
MDcitss <maxdixmatthews@gmail.com>
2021-05-15 19:54:46 +0800

    unittest for addTestScore

--- tests.py
+++ tests.py
@@ -1,12 +1,9 @@
 import unittest, os
 
 from app import app, db
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from app.models import User, TestAttempt
-from datetime import datetime, timedelta
-from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.sql import func
+
 
 # from selenium import webdriver #Not currently used but useful for further testing tim
 # Need statistics testing No dividing by 0
@@ -16,7 +13,6 @@ from sqlalchemy.sql import func
 class UsersTest(unittest.TestCase):
 
     def setUp(self):
-
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all() 
 
@@ -41,24 +37,26 @@ class UsersTest(unittest.TestCase):
         db.session.commit()
         self.assertEqual(u1.username, 'user1')
         self.assertEqual(u2.username, 'user2')
+        self.assertNotEqual(u1.username, 'User1')
+        self.assertNotEqual(u1.username, 'User2')
 
     def test_test_scores(self):
-        u1 = User(username = 'user1',password_hash=generate_password_hash("password1", method='sha256'),)
-        u2 = User(username = 'user2',password_hash=generate_password_hash("password2", method='sha256'))
+        u1 = User(username = 'user1')
+        u2 = User(username = 'user2')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
       
         # Need a test_attempt function!
         # self.assertEqual(len(u1.testAttempts), 0) #Need to make sure test scores start at 0 for all of them
-        t1 = TestAttempt(testId = 1, student = u1)
-        t1_1 = TestAttempt(testId = 1, student = u1)
-        t2 = TestAttempt(testId = 2, student = u1)
-        t3 = TestAttempt(testId = 3, student = u1)
+        t1 = TestAttempt(student = u1)
+        t1_1 = TestAttempt(student = u1)
+        t2 = TestAttempt(student = u1)
+        t3 = TestAttempt(student = u1)
 
         test1u2 = TestAttempt(student = u2)
-        test2u2 = TestAttempt(testId = 2, score = 100,student = u2)
-        test3u2 = TestAttempt(testId = 3, score = 100,student = u2)
+        test2u2 = TestAttempt(student = u2)
+        test3u2 = TestAttempt(student = u2)
 
 
         db.session.add(t1)
@@ -72,16 +70,26 @@ class UsersTest(unittest.TestCase):
         t1.addTestScore(1,10)
         t2.addTestScore(2, 55)
         t1_1.addTestScore(1,100)
-        test = u1.testAttempts.all()
-        for i in test:
-            print(i)
+        testsu1 = u1.testAttempts.all()
 
-        self.assertEqual(test[0].testId, 1)
-        self.assertEqual(test[0].score, 10)
-        self.assertEqual(test[1].score, 100)
-        self.assertNotEqual(test[1],55)
+        # Check that valid inputs are added correctly
+        self.assertEqual(testsu1[0].testId, 1)
+        self.assertEqual(testsu1[0].score, 10)
+        self.assertEqual(testsu1[1].score, 100)
+        self.assertNotEqual(testsu1[1],55)
         self.assertTrue(t1.checkCorrectScore(10))
-        self.assertFalse(t1.checkCorrectScore(0))       
+        self.assertFalse(t1.checkCorrectScore(0))   
+
+        # Check that invalid inputs are not added
+        test1u2.addTestScore(1,-1)
+        test1u2.addTestScore(4,50)
+        test1u2.addTestScore(-1,50)
+        testsu2 = u2.testAttempts.all()
+        for i in range(len(testsu2)):
+            self.assertEqual(testsu2[i].testId, None)
+            self.assertEqual(testsu2[i].score, None)
+        
+
  
         db.session.commit()       
         
